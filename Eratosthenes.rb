03cbbfe751803ec347b2904096f79f4f332ad386
daiki_ikemoto <ikemoto.daiki@gmail.com>
2014-03-15 18:55:25 +0900

    エラトステネスの篩書いた

---
+++ Eratosthenes.rb
@@ -0,0 +1,18 @@
+max = 100
+#2~maxまでの配列をつくる
+ans_array = []
+(2..max).each do |num|
+    ans_array.push(num)
+end
+
+# maxの平方根までループする
+2.upto(Math.sqrt(max)) do |num|
+    y = num
+    # 素数の倍数を削除する
+    while y <= max do
+        y += num 
+        ans_array.delete(y)
+    end
+end
+
+p ans_array
