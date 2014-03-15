482371019b55cbb13decd54a2427787c8cd2b18e
daiki_ikemoto <ikemoto.daiki@gmail.com>
2014-03-15 17:54:54 +0900

    p2

---
+++ p2.rb
@@ -0,0 +1,17 @@
+#フィボナッチ数をつくる
+def fib(n)
+    return 1 if n == 0
+    return 2 if n == 1
+    fib(n - 2) + fib(n - 1)
+end
+
+ans = 0
+num = 0
+max = 4_000_000
+
+while fib(num) <= max 
+    ans += fib(num) if fib(num) % 2 == 0
+    num += 1
+end
+
+p ans
