ecf38113cb74ee74e346e09a5b8504d6accd9072
ikemoto <ikemoto.daiki@gmail.com>
2014-03-16 09:45:39 +0900

    エラトステネスの篩なおした

--- Eratosthenes.rb
+++ Eratosthenes.rb
@@ -1,10 +1,8 @@
 max = 100
 
 #2~maxまでの配列をつくる
-ans_array = []
-(2..max).each do |num|
-    ans_array.push(num)
-end
+ans_array = (2..max).to_a
+# ans_array = [*2..100] この方法でもOK
 
 # maxの平方根までループする
 2.upto(Math.sqrt(max)) do |num|
@@ -12,7 +10,7 @@ end
     # 素数の倍数を削除する
     while y <= max do
         y += num 
-        ans_array.delete(y)
+        ans_array.reject! { |elem| elem % y == 0  }
     end
 end
 
