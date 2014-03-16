6911081f8e55eb0718d625a259298b6cb0102005
ikemoto <ikemoto.daiki@gmail.com>
2014-03-16 10:49:24 +0900

    p4

---
+++ p4.rb
@@ -0,0 +1,36 @@
+# 回文かチェック
+def checkPalindrome(word)
+    word = word.to_s
+    0.upto(word.length / 2) do |num|
+        if word[num] != word[-num - 1]
+            return false 
+        end
+    end
+
+    return true 
+end
+
+# 3桁の数で割り切れるかチェック
+def checkDivided(num)
+    min = 111
+    max = 999
+    # 3桁の積でないものは弾く
+    return false  if num < min ** 2  && num > max ** 2 
+
+    #minから順に割っていき、割り切れる&商が3桁ならtrue
+    min.upto(max) do |x|
+        if num % x == 0 && (num / x).to_s.length == 3
+            return true
+        end
+    end
+
+    return false
+end
+
+max = 999 * 999
+max.downto(1) do |num|
+    if checkPalindrome(num) && checkDivided(num)
+        puts num
+        break
+    end
+end
