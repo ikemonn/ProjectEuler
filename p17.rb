1436b0bb00399a376d2a7495d237a86f83c619ac
daiki_ikemoto <ikemoto.daiki@gmail.com>
2014-03-31 22:40:52 +0900

    p17

---
+++ p17.rb
@@ -0,0 +1,10 @@
+one_to_nine = 36 
+ten_to_nineteen = 70 
+twenty_to_ninty = 10 * (6 + 6 + 5 + 5 + 5 + 7 + 6 + 6) + (8 * 36)
+hundred_to_3nine = (36 * 100) + (9 * 854) + (7 * 9) + (9 * 99 * 10) 
+thousand = 11
+p twenty_to_ninty
+
+p hundred_to_3nine
+
+p one_to_nine + ten_to_nineteen + twenty_to_ninty + hundred_to_3nine + thousand
